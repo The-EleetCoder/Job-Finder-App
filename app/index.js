17dd46c07e9b465bc1489a5694d8c0604ab784d2
The-EleetCoder <jaijain1803@gmail.com>
2023-12-24 22:28:57 +0530

    final commit

--- app/index.js
+++ app/index.js
@@ -11,6 +11,9 @@ import {
 } from "../components";
 
 const Home = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -29,14 +32,23 @@ const Home = () => {
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <View
-            style={{
-                flex: 1,
-                padding: SIZES.medium
-            }}
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
         >
-            <Welcome/>
-            <Popularjobs/>
-            <Nearbyjobs/>
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          />
+
+          <Popularjobs />
+          <Nearbyjobs />
         </View>
       </ScrollView>
     </SafeAreaView>
